1cf311572447ca08259d1db17aa8fd9b79805d07
VeryFamousDroid <diggerspgh@gmail.com>
2015-06-25 16:11:15 -0400

    Added the config file to the repository.

---
+++ blog/config.py
@@ -0,0 +1,9 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://blog:blog_pw1@localhost/blog"
+
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+SECRET_KEY = '<It is a secret>'
+
+WHOOSH_BASE = 'search.db'
+
+DEBUG = True
