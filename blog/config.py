378857f0a7ddfa91090f0bee0c6bbb0dcf192504
ldscavo <logan@scavo.dev>
2020-03-08 14:42:27 -0400

    Updated the app to pull setting from the env

--- blog/config.py
+++ blog/config.py
@@ -1,13 +1,20 @@
-SQLALCHEMY_DATABASE_URI = '<db uri>'
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-SECRET_KEY = '<It is a secret>'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 WHOOSH_BASE = 'search.db'
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-RECAPTCHA_SECRET_KEY = '<insert key here>'
+RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 
-RECAPTCHA_SITE_KEY = '<insert key here>'
+RECAPTCHA_SITE_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
